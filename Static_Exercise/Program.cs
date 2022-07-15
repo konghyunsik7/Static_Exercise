0d6a0c90da980d9be492524432d6f84ff9858df0
Hyun Kong <konghyunsik7@gmail.com>
2022-07-15 18:12:47 -0400

    updated

---
+++ Static_Exercise/Program.cs
@@ -0,0 +1,23 @@
+﻿//Create a new console app called StaticExercise.
+
+//Now create a static class called TempConverter.The class should have at least 2 methods, one called FahrenheitToCelsius that will require a double as a parameter and return a double, the other CelsiusToFahrenheit which will also require a double as a parameter and return a double.
+
+//Fill out these methods and call them in your Program.cs file. Make sure they’re accurate!
+
+using Static_Exercise;
+
+Console.WriteLine("Please Enter the Celsius Temp: ");
+double userInput= double.Parse(Console.ReadLine());
+double celcius = TempConvertor.FahrenheitToCelsius(userInput);
+
+
+Console.WriteLine("Sweeeeeet! Now please enter the Fahrenheit temp: ");
+double userInput2 = double.Parse(Console.ReadLine());
+double fahrenheit = TempConvertor.CelsiusToFahrenheit(userInput2);
+Console.WriteLine();
+
+Console.WriteLine($"Here are the temp for Celsius and Fahrenheit:{celcius} & {fahrenheit}");
+
+
+
+
---
+++ Static_Exercise/TempConvertor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Exercise
+{
+    public static class TempConvertor
+    {
+        //subtract 32 and multiply by . 5556 (or 5/9).
+        public static double FahrenheitToCelsius(double temp) 
+        {
+            return (temp - 32) / 1.8;
+        }
+        //multiply by 1.8 (or 9/5) and add 32.
+        public static double CelsiusToFahrenheit(double temp)
+        {
+            return (temp * 1.8) + 32;
+        }
+    }
+}
